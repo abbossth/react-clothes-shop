a1ba702cc872a0fbf26de6404fd3e730bbd6687e
abbossth <bosskodevelopment@gmail.com>
2022-06-17 19:57:05 +0500

    React Router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Home from "./routes/home/home.component";
 import { Routes, Route } from "react-router-dom";
-
 import Navigation from "./routes/navigation/navigation.component";
 
 const Shop = () => {
@@ -11,9 +10,9 @@ const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
+        <Route path="/" element={<h1>Welcome to Ecommerce!</h1>}/>
+        <Route index path="/home" element={<Home />}></Route>
         <Route path="/shop" element={<Shop />}></Route>
-        <Route path="/home" element={<Home />}></Route>
-        <Route path="/main" element={<h1>Main</h1>}></Route>
       </Route>
     </Routes>
   );
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,29 +1,28 @@
-import { Fragment } from 'react';
-import { Outlet, Link } from 'react-router-dom'
+import "./navigation.styles.scss";
+import { Link, Outlet } from "react-router-dom";
+import { Fragment } from "react";
 
-import "./navigation.styles.scss"
-
-import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 
 const Navigation = () => {
-    return (
-        <Fragment>
-            <div className="navigation">
-                <Link to='/' className="logo-container">
-                    <CrwnLogo className="logo" />
-                </Link>
-                <div className="nav-links-container">
-                    <Link to='/home' className="nav-link">
-                        HOME
-                    </Link>
-                    <Link to='/shop' className="nav-link">
-                        SHOP
-                    </Link>
-                </div>
-            </div>
-            <Outlet />
-        </Fragment>
-    );  
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link to="/" className="logo-container">
+          <CrwnLogo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link to="/home" className="nav-link">
+            Home
+          </Link>
+          <Link to="/shop" className="nav-link">
+            Shop
+          </Link>
+        </div>
+      </div>
+      <Outlet></Outlet>
+    </Fragment>
+  );
 };
 
 export default Navigation;
