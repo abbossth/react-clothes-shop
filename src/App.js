2735efc3de972c46a6d865f183716d628e49693e
00011733 <bosskodevelopment@gmail.com>
2022-07-24 02:02:57 +0500

    Shop context and new component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
-import { useContext } from "react";
-import { UserContext } from "./contexts/user.context";
-
-const Shop = () => {
-  const { currentUser } = useContext(UserContext);
-  return <h1>Welcome to Shop{currentUser !== null ? ', ' + currentUser.displayName : ''} </h1>;
-};
+import Shop from "./routes/shop/shop.component";
 
 const App = () => {
   return (
