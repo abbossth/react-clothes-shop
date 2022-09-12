8b589be77b60938bdb4211aa958e284dc756b2e8
abbossth <bosskodevelopment@gmail.com>
2022-09-13 03:39:36 +0500

    checkout to styled components

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,29 +1,32 @@
-import "./checkout-item.styles.scss"
 import { useContext } from "react"
 import { CartContext } from "../../contexts/cart.context"
+
+
+import { CheckoutItemContainer, ImageContainer, Name, Quantity, Price, Arrow, Value, RemoveButton } from "./checkout-item.styles"
+
 
 const CheckoutItem = ({ cartItem, addItemToCart, removeItemFromCart }) => {
   const { name, quantity, imageUrl, price } = cartItem
   const { clearItemFromCart } = useContext(CartContext)
 
   return (
-    <div className="checkout-item-container">
-      <div className="image-container">
+    <CheckoutItemContainer>
+      <ImageContainer>
         <img src={imageUrl} alt={`${name}`} />
-      </div>
-      <span className="name"> {name} </span>
-      <span className="quantity">
-        <div className="arrow" onClick={() => removeItemFromCart(cartItem)}>
+      </ImageContainer>
+      <Name> {name} </Name>
+      <Quantity>
+        <Arrow onClick={() => removeItemFromCart(cartItem)}>
          &#10094;
-        </div>
-        <span className="value">{quantity}</span>
-        <div className="arrow" onClick={() => addItemToCart(cartItem)}>
+        </Arrow>
+        <Value>{quantity}</Value>
+        <Arrow onClick={() => addItemToCart(cartItem)}>
           &#10095;
-        </div>
-      </span>
-      <span className="price"> {price} </span>
-      <span className="remove-button" onClick={() => clearItemFromCart(cartItem)}>&#10005;</span>
-    </div>
+        </Arrow>
+      </Quantity>
+      <Price> {price} </Price>
+      <RemoveButton onClick={() => clearItemFromCart(cartItem)}>&#10005;</RemoveButton>
+    </CheckoutItemContainer>
   )
 }
 
